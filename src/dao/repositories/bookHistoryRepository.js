5307bcb7812062cf85d45b6f410023b0c1efefdb
pokers <pokers@pokersui-iMac-Pro.local>
2023-03-20 20:24:54 +0900

    BMOL-25 fixed bugs and refactorying

--- src/dao/repositories/bookHistoryRepository.js
+++ src/dao/repositories/bookHistoryRepository.js
@@ -1,6 +1,5 @@
 const pgClient = require('../connections/postgresql')
 
-
 class BookHistoryRepository {
     constructor(){
         this._repositoryName = 'BookHistoryRepository'
@@ -11,25 +10,14 @@ class BookHistoryRepository {
 
 
     async getBookHistoryListByBookId(bookId){
+        // 같은 column 이름을 구분하기 위해 deleted_at에 alias 설정
         const query = pgClient.select('*')
                         .from('tbl_mybook as tb')
                         .join('tbl_myhistory as th', 'tb.id','=','th.mybook_id')
                         .where('tb.id', bookId)
                         .whereNull('tb.deleted_at')
                         .whereNull('th.deleted_at')
-                        .orderBy('th.updated_at', 'desc')
-                        .limit(100)
-
-        return await query
-    }
-
-    async getBookHistoryByBookHistoryId(bookHistoryId){
-        const query = pgClient.select('*')
-                        .from('tbl_myhistory as th')
-                        .where('th.id', bookHistoryId)
-                        .whereNull('th.deleted_at')
                         .first()
-
 
         return await query
     }
@@ -42,49 +30,6 @@ class BookHistoryRepository {
                         .first()
         return await query
     }
-
-    async insertReadingTimeByBookId(userId, bookId, readingTime){
-        
-        const query = pgClient.transaction(function(trx) {
-                                        pgClient.insert({id:pgClient.raw('gen_random_uuid()') , 
-                                        user_id : userId, 
-                                        mybook_id: bookId, 
-                                        reading_time: readingTime})
-                                        .into('tbl_myhistory')
-                                        .returning('id')
-                                        .then(trx.commit) 
-                                        .catch(trx.rollback)});
-
-        return await query
-
-
-    }
-    async removeReadingTimeByBookHistoryId(bookHistoryId){
-        
-        const query = pgClient.transaction(function(trx) {
-                                        pgClient.table('tbl_myhistory')
-                                        .update({deleted_at : pgClient.raw('now()')})
-                                        .where('id', bookHistoryId)
-                                        .then(trx.commit) 
-                                        .catch(trx.rollback)});
-
-        return await query
-
-
-    }
-
-    async removeReadingTimeByBookId(bookId){
-        const query = pgClient.transaction(function(trx) {
-                                        pgClient.table('tbl_myhistory')
-                                        .update({deleted_at : pgClient.raw('now()')})
-                                        .where('mybook_id', bookId)
-                                        .then(trx.commit) 
-                                        .catch(trx.rollback)});
-        return await query
-
-
-    }
-
 }
 
 module.exports = {
